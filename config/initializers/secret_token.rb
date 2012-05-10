52388ab50baf3cb49c61268a2427d172d906eff5
高尾 宏治 <kouji@netlab.jp>
2012-05-10 16:25:56 +0900

    rails new doya -d mysqlを実行

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doya::Application.config.secret_token = 'fcd657a54388be493c8839b8e841cb0fceb2d382be499d2bf4e8a627cf7356166e9aeaf21a8a2b89a0fffbf2c26bc0c2f8d16fd39149b37c83c48085cf728bc5'
